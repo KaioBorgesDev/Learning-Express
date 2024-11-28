449a0dc59e650e6730d0a7e45b7e3e1546dfac1c
Monolokaio <kaioeduardo401@outlook.com>
2024-11-28 14:28:56 -0300

    feat- iniciando projeto com mongoose

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./src/models/User');
 const app = express();
+const uri = 'mongodb://localhost:27017/learn_express';
 
-app.get("/", function(req,res){
-    res.send("Hello World");
+mongoose.connect(uri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
+
+const createUserController = (req, res) =>{
+    return User.create(req.body)
+    .then(user => res.status(201).json(user))
+    .catch(err => res.status(500).json({erros: [err.message]}));
+}
+app.post("/api/user", createUserController)
 
 app.listen(3000)
